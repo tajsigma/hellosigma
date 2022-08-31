371f7d03ecb91e575eb7242046c407710f451068
tajsigma <112558994+tajsigma@users.noreply.github.com>
2022-08-31 17:50:34 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-alert("Hello world");
+const http = require('http');
+
+const requestListener = function (req, res) {
+  res.writeHead(200);
+  res.end('Hello, World!');
+}
+
+const server = http.createServer(requestListener);
+server.listen(8080);
